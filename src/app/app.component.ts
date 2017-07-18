2c6ffc0f07ffdba0b19d82994ec8e5e231070dfa
Troy Center <troycenter1@gmail.com>
2017-07-17 18:47:57 -0700

    finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Registration Form';
+  isSuccessful: boolean;
+  user = new User();
+  states: Array<string> = [];  
+  
+  constructor() {
+    this.user = new User();
+    this.isSuccessful = false;
+    this.states = ["Alaska",
+                      "Alabama",
+                      "Arkansas",
+                      "Arizona",
+                      "California",
+                      "Colorado",
+                      "Connecticut",
+                      "District of Columbia",
+                      "Delaware",
+                      "Florida",
+                      "Georgia",
+                      "Hawaii",
+                      "Iowa",
+                      "Idaho",
+                      "Illinois",
+                      "Indiana",
+                      "Kansas",
+                      "Kentucky",
+                      "Louisiana",
+                      "Massachusetts",
+                      "Maryland",
+                      "Maine",
+                      "Michigan",
+                      "Minnesota",
+                      "Missouri",
+                      "Mississippi",
+                      "Montana",
+                      "North Carolina",
+                      "North Dakota",
+                      "Nebraska",
+                      "New Hampshire",
+                      "New Jersey",
+                      "New Mexico",
+                      "Nevada",
+                      "New York",
+                      "Ohio",
+                      "Oklahoma",
+                      "Oregon",
+                      "Pennsylvania",
+                      "Rhode Island",
+                      "South Carolina",
+                      "South Dakota",
+                      "Tennessee",
+                      "Texas",
+                      "Utah",
+                      "Virginia",
+                      "Vermont",
+                      "Washington",
+                      "Wisconsin",
+                      "West Virginia",
+                      "Wyoming"]
+      }
+
+      onSubmit() {
+        console.log("submitting...");
+        this.isSuccessful = true;
+        this.user = new User()
+    };
 }
